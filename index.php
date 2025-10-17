80f851558af889ca59e96b153d14e85abbdc7a31
Kauã Nunes <kauanunes.dev09@gmail.com>
2025-10-17 20:37:09 -0300

    commit inicial do sistema de chamados - Configurações css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <main>
+    <section class="left">
+      <img src="img/img-login.png" alt="login ilustrativo">
+    </section>
+
+    <section class="right">
+      <form action="" method="post">
+        <label for="email">E-Mail</label>
+        <input type="email" id="email" required>
+
+        <label for="senha">senha</label>
+        <input type="password" id="senha" required>
+
+        <button type="submit">LOGIN</button>
+
+        <p>não tem uma conta? <a href="pages/cadastro.php">cadastre-se</a></p>
+      </form>
+    </section>
+  </main>
+</body>
+</html>
---
+++ pages/cadastro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tela de Cadastro</title>
+  <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+  <main>
+    <section class="left">
+      <img src="https://cdn-icons-png.flaticon.com/512/7856/7856540.png" alt="ilustração de cadastro">
+    </section>
+
+    <section class="right">
+      <form>
+        <label for="nome">Nome completo</label>
+        <input type="text" id="nome" required>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" required>
+
+        <label for="senha">Senha</label>
+        <input type="password" id="senha" required>
+
+        <label for="confirma">Confirmar senha</label>
+        <input type="password" id="confirma" required>
+
+        <label for="celular">Celular</label>
+        <input type="tel" id="celular" required>
+
+        <label for="cargo">Cargo</label>
+        <select name="cargo" id="cargo" required>
+            <option value="#"></option>
+        </select>
+
+        <button type="submit">cadastre-se</button>
+
+        <p>já tem uma conta? <a href="../index.php">faça login</a></p>
+      </form>
+    </section>
+  </main>
+</body>
+</html>
