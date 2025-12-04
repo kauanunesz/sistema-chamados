97a78df4c106f048aa225db0692d256a09bef524
Kauã Nunes <kauanunes.dev09@gmail.com>
2025-12-04 13:43:52 -0300

    alterações

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
     $server = "localhost";
     $user = "root";
-    $password = "root";
+    $password = "808801!";
     $database = "bd_chamados";
     
     $conexao = new mysqli($server, $user, $password, $database);
